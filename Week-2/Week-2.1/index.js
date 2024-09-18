9d459802f052e4e46772664de23a2f81ed1cc8f3
hey pritesh <priteshdhanad2001@gmail.com>
2024-09-18 21:51:31 +0530

    Week 1 to Week 3 Added ( Mostly Basic FE Part )

---
+++ Week-2/Week-2.1/index.js
@@ -0,0 +1,10 @@
+/* const { log } = require("console");
+const fs = require("fs"); // external libraries
+
+const data = fs.readFileSync("a.txt", "utf8"); // utf is encoding which undersrood by humans ( eng lang )
+console.log(data);
+
+const data2 = fs.readFileSync("b.txt", "utf8"); 
+console.log(data2);
+ */
+
