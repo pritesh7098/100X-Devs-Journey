e7decd3b2f435e0b4a073040579b0bc94983c1ac
hey pritesh <priteshdhanad2001@gmail.com>
2024-09-26 18:12:39 +0530

    Updated user and admin routes with buisness logic

--- Week-8/Week-8.1/routes/adminRoute.js
+++ Week-8/Week-8.1/routes/adminRoute.js
@@ -1,15 +1,50 @@
 const { Router } = require("express");
-const adminRouter = Router();
 const { adminModel } = require("../db");
-adminRouter.post("/signup", function (req, res) {
+const { jwt } = require("jsonwebtoken");
+const JWT_KEY = "PriteshAdmin70982001";
+
+const adminRouter = Router();
+
+adminRouter.post("/signup", async function (req, res) {
+  const { email, password, firstName, lastName } = req.body; // u can use another way also but it it good and short
+
+  await adminModel.create({
+    email: email,
+    password: password,
+    firstName: firstName,
+    lastName: lastName,
+  });
+
   res.json({
-    message: " Admin signup successfully",
+    message: "Admin Signup Successfully ",
   });
 });
 
 adminRouter.post("/signin", function (req, res) {
+  const { email, password } = req.body;
+
+  // the user which we have to check if he is registered on our db/site or not
+
+  let user = adminModel.findOne({
+    email,
+    password,
+  });
+
+  if (user) {
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      JWT_KEY
+    );
+  } else {
+    res.status(403).json({
+      message: " Unauthorized Admin spotted bhag bc ",
+    });
+  }
+
   res.json({
-    message: "Admin Signin successfully",
+    message: "Admin Login Successfully",
   });
 });
 
--- Week-8/Week-8.1/routes/userRoute.js
+++ Week-8/Week-8.1/routes/userRoute.js
@@ -1,16 +1,54 @@
+const express = require("express");
 const { Router } = require("express");
-const userRouter = Router();
 const { userModel } = require("../db");
+const { jwt } = require("jsonwebtoken");
+const JWT_KEY = "Pritesh70982001";
 
-userRouter.post("/signup", function (req, res) {
+const userRouter = Router();
+
+const app = express();
+app.use(express.json());
+
+userRouter.post("/signup", async function (req, res) {
+  const { email, password, firstName, lastName } = req.body; // u can use another way also but it it good and short
+
+  await userModel.create({
+    email: email,
+    password: password,
+    firstName: firstName,
+    lastName: lastName,
+  });
+
   res.json({
-    message: "Signup succeeded",
+    message: "User Signup Successfully ",
   });
 });
 
 userRouter.post("/signin", function (req, res) {
+  const { email, password } = req.body;
+
+  // the user which we have to check if he is registered on our db/site or not
+
+  let user = userModel.findOne({
+    email,
+    password,
+  });
+
+  if (user) {
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      JWT_KEY
+    );
+  } else {
+    res.status(403).json({
+      message: " Unauthorized user spotted bhag bc ",
+    });
+  }
+
   res.json({
-    message: "Signup succeeded",
+    message: "User Login Successfully",
   });
 });
 
