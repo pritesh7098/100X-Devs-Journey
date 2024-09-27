0895733bb87b0dd95b3dd7c5cdee0edf8c5c3ae9
hey pritesh <priteshdhanad2001@gmail.com>
2024-09-27 18:10:45 +0530

    Updated user and admin routes with buisness logic again

--- Week-8/Week-8.1/routes/adminRoute.js
+++ Week-8/Week-8.1/routes/adminRoute.js
@@ -1,7 +1,8 @@
 const { Router } = require("express");
-const { adminModel } = require("../db");
+const { adminModel, courseModel } = require("../db");
 const { jwt } = require("jsonwebtoken");
-const JWT_KEY = "PriteshAdmin70982001";
+const { ADMIN_JWT_KEY } = require("../config");
+const { AdminMiddleware } = require("../middlewares/AdminMiddleware");
 
 const adminRouter = Router();
 
@@ -35,22 +36,57 @@ adminRouter.post("/signin", function (req, res) {
       {
         id: user._id,
       },
-      JWT_KEY
+      ADMIN_JWT_KEY
     );
+    res.json({
+      message: "Admin Login Successfully",
+      token: token, // token will be sent in response with other data
+    });
   } else {
     res.status(403).json({
-      message: " Unauthorized Admin spotted bhag bc ",
+      message: "Unauthorized Admin spotted bhag bc ",
     });
   }
+});
+
+adminRouter.post("/postcourse", AdminMiddleware, async function (req, res) {
+  const adminId = req.userId; // here adminId is nothing but id which is verified in admin middleware
+
+  // basically we have to craete a new course and add it to databse as an admin
+
+  const { title, description, price, imageUrl } = req.body;
+
+  const course = await courseModel.create({
+    title: title,
+
+    description: description,
+
+    price: price,
+
+    imageUrl: imageUrl,
+
+    creatorId: adminId, // this id will be the id of admin who created this course.
+  });
 
   res.json({
-    message: "Admin Login Successfully",
+    message: " Course creation successfully by Admin",
+    courseId: course._id,
   });
 });
 
-adminRouter.post("/createCourses", function (req, res) {
+adminRouter.put("/update", async function (req, res) {
+  const { title, description, price, imageUrl } = req.body;
+
+  const updateCourse = await courseModel.updateOne({});
+
   res.json({
-    message: " Course creation successfully",
+    message: " Course Updation successfully by Admin",
+  });
+});
+
+adminRouter.delete("/getallcourses", function (req, res) {
+  res.json({
+    message: " Course Deletion successfully by Admin",
   });
 });
 
--- Week-8/Week-8.1/routes/userRoute.js
+++ Week-8/Week-8.1/routes/userRoute.js
@@ -2,15 +2,23 @@ const express = require("express");
 const { Router } = require("express");
 const { userModel } = require("../db");
 const { jwt } = require("jsonwebtoken");
-const JWT_KEY = "Pritesh70982001";
-
+const { USER_JWT_KEY } = require("../config");
 const userRouter = Router();
 
 const app = express();
 app.use(express.json());
-
 userRouter.post("/signup", async function (req, res) {
+  console.log(req.body); // Log the request body
+
   const { email, password, firstName, lastName } = req.body; // u can use another way also but it it good and short
+
+  // handling error when nofield is provided
+
+  if (!email || !password || !firstName || !lastName) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  // creating/ adding a new user entry in database
 
   await userModel.create({
     email: email,
@@ -24,32 +32,41 @@ userRouter.post("/signup", async function (req, res) {
   });
 });
 
-userRouter.post("/signin", function (req, res) {
+userRouter.post("/signin", async function (req, res) {
   const { email, password } = req.body;
+
+  // handling error when nofield is provided
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email and password are required" });
+  }
 
   // the user which we have to check if he is registered on our db/site or not
 
-  let user = userModel.findOne({
+  let user = await userModel.findOne({
     email,
     password,
   });
+
+  // if user is is our database toh karo token generate bc
 
   if (user) {
     const token = jwt.sign(
       {
         id: user._id,
       },
-      JWT_KEY
+      USER_JWT_KEY
     );
+
+    res.json({
+      message: "User Login Successfully",
+      token: token,
+    });
   } else {
     res.status(403).json({
       message: " Unauthorized user spotted bhag bc ",
     });
   }
-
-  res.json({
-    message: "User Login Successfully",
-  });
 });
 
 module.exports = {
