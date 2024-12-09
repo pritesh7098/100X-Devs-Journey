57169c778be670deb5ee0e4266fc52559e19ea29
hey_pritesh <priteshdhanad2001@gmail.com>
2024-12-09 19:44:02 +0530

    removed connection string

--- Week-18/Week-18.1-postgres/src/index.ts
+++ Week-18/Week-18.1-postgres/src/index.ts
@@ -3,9 +3,8 @@ import { Client } from "pg"; // importing pg lib
 // same as defining express we create a object/instance from a class Client.
 // either u can put string or a whole object.
 
-let client = new Client(
-  "postgresql://neondb_owner:7K1HJntqVWBG@ep-restless-heart-a5humq7n.us-east-2.aws.neon.tech/neondb?sslmode=require"
-);
+let client = new Client();
+// connection string here
 
 // basic fun to connect with the database and querying the database.
 
@@ -16,4 +15,3 @@ async function main() {
 }
 
 main();
-
