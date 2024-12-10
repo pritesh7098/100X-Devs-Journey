b0f98db197b1c0620d7254f3a2a4d089f654f838
hey_pritesh <priteshdhanad2001@gmail.com>
2024-12-10 11:15:30 +0530

    Done with basic pg usage and created an simple todo app

--- Week-18/Week-18.1-postgres/dist/index.js
+++ Week-18/Week-18.1-postgres/dist/index.js
@@ -8,14 +8,84 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const pg_1 = require("pg");
-let client = new pg_1.Client("postgresql://neondb_owner:7K1HJntqVWBG@ep-restless-heart-a5humq7n.us-east-2.aws.neon.tech/neondb?sslmode=require");
-function main() {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield client.connect();
-        const res = yield client.query("select * from users");
-        console.log(res.rows);
-    });
+const pg_1 = require("pg"); // importing pg lib
+// same as defining express we create a object/instance from a class Client.
+// either u can put string or a whole object.
+/* let client = new Client(
+  "postgresql://neondb_owner:7K1HJntqVWBG@ep-restless-heart-a5humq7n.us-east-2.aws.neon.tech/neondb?sslmode=require"
+);
+// connection string here
+
+// basic fun to connect with the database and querying the database.
+
+async function main() {
+  await client.connect();
+  const res = await client.query("select * from users");
+  console.log(res.rows);
 }
-main();
+
+main(); */
+/* **************************************************************************************************** */
+// Create a simple Node.js app that lets you put data
+// Create a function that let’s you insert data into a table. Make it async, make sure client.connect resolves before u do the insert
+// Async function to insert data into a table
+/* async function insertData() {
+  const client = new Client(
+    "postgresql://neondb_owner:7K1HJntqVWBG@ep-restless-heart-a5humq7n.us-east-2.aws.neon.tech/neondb?sslmode=require"
+  );
+
+  try {
+    await client.connect(); // Ensure client connection is established
+    const insertQuery =
+      "INSERT INTO users (username, email, password) VALUES ('Mami', 'Mami@example.com', 'mami_password123');";
+    const res = await client.query(insertQuery);
+    console.log("Insertion success:", res); // Output insertion result
+  } catch (err) {
+    console.error("Error during the insertion:", err);
+  } finally {
+    await client.end(); // Close the client connection
+  }
+}
+
+insertData();
+ */
+/* **************************************************************************************************** */
+// Create a simple Node.js todo app with postgresql like when the user signup with his cred the cred will store in our pg db
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+// Connect to the PostgreSQL database
+const pgClient = new pg_1.Client("postgresql://neondb_owner:7K1HJntqVWBG@ep-restless-heart-a5humq7n.us-east-2.aws.neon.tech/neondb?sslmode=require");
+pgClient.connect();
+// Create a new user
+app.post("/signup", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+    try {
+        const insertQuery = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3);`;
+        const response = yield pgClient.query(insertQuery, [
+            username,
+            email,
+            password,
+        ]);
+        res.json({
+            message: "You have signed up successfully ",
+        });
+    }
+    catch (e) {
+        console.log(e);
+        res.json({
+            message: "Error while signing up",
+        });
+    }
+}));
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+/* **************************************************************************************************** */
--- Week-18/Week-18.1-postgres/src/index.ts
+++ Week-18/Week-18.1-postgres/src/index.ts
@@ -3,8 +3,10 @@ import { Client } from "pg"; // importing pg lib
 // same as defining express we create a object/instance from a class Client.
 // either u can put string or a whole object.
 
-let client = new Client();
+/* let client = new Client(
 // connection string here
+
+);
 
 // basic fun to connect with the database and querying the database.
 
@@ -14,4 +16,87 @@ async function main() {
   console.log(res.rows);
 }
 
-main();
+main(); */
+
+/* **************************************************************************************************** */
+
+// Create a simple Node.js app that lets you put data
+// Create a function that let’s you insert data into a table. Make it async, make sure client.connect resolves before u do the insert
+
+// Async function to insert data into a table
+/* async function insertData() {
+  const client = new Client(
+// connection string here
+
+  );
+
+  try {
+    await client.connect(); // Ensure client connection is established
+    const insertQuery =
+      "INSERT INTO users (username, email, password) VALUES ('Mami', 'Mami@example.com', 'mami_password123');";
+    const res = await client.query(insertQuery);
+    console.log("Insertion success:", res); // Output insertion result
+  } catch (err) {
+    console.error("Error during the insertion:", err);
+  } finally {
+    await client.end(); // Close the client connection
+  }
+}
+
+insertData();
+ */
+
+/* **************************************************************************************************** */
+
+// Create a simple Node.js todo app with postgresql like when the user signup with his cred the cred will store in our pg db
+
+/* import express from "express";
+
+const app = express();
+app.use(express.json());
+
+// Connect to the PostgreSQL database
+const pgClient = new Client(
+
+// connection string here
+
+
+);
+
+pgClient.connect();
+
+// Create a new user
+
+app.post("/signup", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const email = req.body.email;
+
+  try {
+    const insertQuery = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3);`;
+
+    const response = await pgClient.query(insertQuery, [
+      username,
+      email,
+      password,
+    ]);
+
+    res.json({
+      message: "You have signed up successfully ",
+    });
+  } catch (e) {
+    console.log(e);
+    res.json({
+      message: "Error while signing up",
+    });
+  }
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+ */
+
+/* **************************************************************************************************** */
